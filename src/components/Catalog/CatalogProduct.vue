d35cddf77c1c7581963545afecc4b4351965f868
witold1885 <wiktor8555@gmail.com>
2023-02-03 20:44:45 +0200

    Change catalog product card markup

--- src/components/Catalog/CatalogProduct.vue
+++ src/components/Catalog/CatalogProduct.vue
@@ -1,14 +1,36 @@
 <template>
-	<div class="catalog-product d-flex flex-column">
-		<img class="catalog-product__image" :src="require(`@/assets/images/products/${product.image.replace('.png', '-' + breakpoint + '.png')}`)" />
-		<div class="catalog-product__subcategory">{{ product.subcategory }}</div>
-		<div class="catalog-product__title">{{ product.title }}</div>
-		<div class="catalog-product__bottom d-flex justify-content-between">
-			<div class="catalog-product__price d-flex flex-column">
-				<div class="catalog-product__price-main">{{ product.price }}</div>
-				<div class="catalog-product__price-old">{{ product.oldPrice }}</div>
-			</div>
+	<div class="catalog-product d-flex flex-column justify-content-between">
+		<div class="catalog-product__up">
+			<div class="catalog-product__date">{{ product.date }}</div>
 			<FavoritesButton class="catalog-product__favorite" border-color="green"/>
+			<img class="catalog-product__image" :src="require(`@/assets/images/products/${product.image.replace('.png', '-' + breakpoint + '.png')}`)" />
+			<div class="catalog-product__middle d-flex justify-content-between align-items-center">
+				<div class="catalog-product__subcategory">{{ product.subcategory }}</div>
+				<div class="catalog-product__location d-flex justify-content-end align-items-center">
+					<img class="catalog-product__location-icon" :src="require('@/assets/images/location.png')" />
+					<div class="catalog-product__location-value">{{ product.location }}</div>
+				</div>
+			</div>
+			<div class="catalog-product__title">{{ product.title }}</div>
+		</div>
+		<div class="catalog-product__down">
+			<div class="catalog-product__divider"></div>
+			<div class="catalog-product__bottom d-flex flex-column justify-content-end">
+				<div class="catalog-product__price d-flex flex-column">
+					<div class="catalog-product__price-main">{{ product.price }}</div>
+					<div class="catalog-product__price-old">{{ product.oldPrice }}</div>
+				</div>
+				<div class="catalog-product__stats d-flex justify-content-between align-items-center">
+					<div class="catalog-product__stats-status d-flex align-items-center">
+						<img class="catalog-product__stats-status-icon" :src="require('@/assets/images/ok.png')" />
+						<div class="catalog-product__stats-status-value">{{ product.status }}</div>
+					</div>
+					<div class="catalog-product__stats-views d-flex justify-content-end align-items-center">
+						<img class="catalog-product__stats-views-icon" :src="require('@/assets/images/eye.png')" />
+						<div class="catalog-product__stats-views-value">{{ product.views }}</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
--- src/components/Catalog/products.js
+++ src/components/Catalog/products.js
@@ -4,56 +4,88 @@ const products = [
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	},
 	{
 		subcategory: 'Сеялки',
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	},
 	{
 		subcategory: 'Сеялки',
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	},
 	{
 		subcategory: 'Сеялки',
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	},
 	{
 		subcategory: 'Сеялки',
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	},
 	{
 		subcategory: 'Сеялки',
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	},
 	{
 		subcategory: 'Сеялки',
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	},
 	{
 		subcategory: 'Сеялки',
 		title: '8-ми рядковая анкерная пневматическая сеялка',
 		price: '1 581 643 тенге',
 		oldPrice: '1 581 643 тенге',
-		image: 'last-product.png'
+		image: 'last-product.png',
+		location: 'Усть-Каменогорск',
+		date: '12.01.2023',
+		status: 'Новый',
+		views: 324
 	}
 ]
 
