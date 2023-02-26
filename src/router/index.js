6ed743841b19984be0421b6971901819eba370c0
witold1885 <wiktor8555@gmail.com>
2023-02-26 02:25:46 +0200

    Router scroll to top

--- src/router/index.js
+++ src/router/index.js
@@ -56,7 +56,12 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  // eslint-disable-next-line
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  }
 })
 
 export default router
