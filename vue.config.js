375ec04a9b40b76a0226e45d0eea22b1d255bf7f
witold1885 <wiktor8555@gmail.com>
2023-03-29 17:11:27 +0300

    Webpack AMP config

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,33 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HTMLInlineCSSWebpackPlugin({
+        styleTagFactory({ style }) {
+          return `<style amp-custom>${style}</style>`;
+        },
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+          ]
+        }
+      ]
+    }
+  }
+}
+
