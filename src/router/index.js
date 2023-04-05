70e5f89fc1d024a4bd1c1b76dc0d0f26f57881fa
witold1885 <wiktor8555@gmail.com>
2023-04-06 01:39:49 +0300

    Divide routes into separate chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,102 +1,115 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import CatalogView from '../views/CatalogView.vue'
-import CompleteRegister from '../views/Profile/CompleteRegister.vue'
-import Preferences from '../views/Profile/Preferences.vue'
-import ProfileProducts from '../views/Profile/Products.vue'
-import ProfileFavorites from '../views/Profile/Favorites.vue'
-import ResetPassword from '../views/Actions/ResetPassword.vue'
-import CreateProduct from '../views/Product/CreateProduct.vue'
-import EditProduct from '../views/Product/EditProduct.vue'
-import ProductView from '../views/ProductView.vue'
-import SearchView from '../views/SearchView.vue'
-import BlogView from '../views/BlogView.vue'
-import Article from '../views/Blog/Article.vue'
-import NotFoundView from '../views/NotFoundView.vue'
+// import HomeView from '../views/HomeView.vue'
+// import CatalogView from '../views/CatalogView.vue'
+// import CompleteRegister from '../views/Profile/CompleteRegister.vue'
+// import Preferences from '../views/Profile/Preferences.vue'
+// import ProfileProducts from '../views/Profile/Products.vue'
+// import ProfileFavorites from '../views/Profile/Favorites.vue'
+// import ResetPassword from '../views/Actions/ResetPassword.vue'
+// import CreateProduct from '../views/Product/CreateProduct.vue'
+// import EditProduct from '../views/Product/EditProduct.vue'
+// import ProductView from '../views/ProductView.vue'
+// import SearchView from '../views/SearchView.vue'
+// import BlogView from '../views/BlogView.vue'
+// import Article from '../views/Blog/Article.vue'
+// import NotFoundView from '../views/NotFoundView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue'),
+    // component: HomeView
+  },
+  {
+    path: '/catalog/:category?',
+    name: 'catalog',
+    component: () => import(/* webpackChunkName: "catalog" */ '../views/CatalogView.vue'),
+    // component: CatalogView
   },
   {
     path: '/profile',
     name: 'profile',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ProfileView.vue'),
+    component: () => import(/* webpackChunkName: "profile" */ '../views/ProfileView.vue'),
     redirect: '/profile/preferences',
     children: [
       {
         path: 'complete-register',
-        component: CompleteRegister
+        component: () => import(/* webpackChunkName: "profile-complete-register" */ '../views/Profile/CompleteRegister.vue'),
+        // component: CompleteRegister
       },
       {
         path: 'preferences',
-        component: Preferences
+        component: () => import(/* webpackChunkName: "profile-preferences" */ '../views/Profile/Preferences.vue'),
+        // component: Preferences
       },
       {
         path: 'products/:status?',
-        component: ProfileProducts
+        component: () => import(/* webpackChunkName: "profile-products" */ '../views/Profile/Products.vue'),
+        // component: ProfileProducts
       },
       {
         path: 'favorites',
-        component: ProfileFavorites
+        component: () => import(/* webpackChunkName: "profile-favorites" */ '../views/Profile/Favorites.vue'),
+        // component: ProfileFavorites
       },
     ]
   },
   {
     path: '/reset-password/:token?',
     name: 'reset-password',
-    component: ResetPassword
-  },
-  {
-    path: '/catalog/:category?',
-    name: 'catalog',
-    component: CatalogView
+    component: () => import(/* webpackChunkName: "reset-password" */ '../views/Actions/ResetPassword.vue'),
+    // component: ResetPassword
   },
   {
     path: '/create-product',
     name: 'create-product',
-    component: CreateProduct
+    component: () => import(/* webpackChunkName: "create-product" */ '../views/Product/CreateProduct.vue'),
+    // component: CreateProduct
   },
   {
     path: '/edit-product/:product_id',
     name: 'edit-product',
-    component: EditProduct
+    component: () => import(/* webpackChunkName: "edit-product" */ '../views/Product/EditProduct.vue'),
+    // component: EditProduct
   },
   {
     path: '/product/:product?',
     name: 'product',
-    component: ProductView
+    component: () => import(/* webpackChunkName: "product" */ '../views/ProductView.vue'),
+    // component: ProductView
   },
   {
     path: '/search',
     name: 'search',
-    component: SearchView
+    component: () => import(/* webpackChunkName: "search" */ '../views/SearchView.vue'),
+    // component: SearchView
   },
   {
     path: '/blog',
     name: 'blog',
-    component: BlogView
+    component: () => import(/* webpackChunkName: "blog" */ '../views/BlogView.vue'),
+    // component: BlogView
   },
   {
     path: '/blog/:article',
     name: 'article',
-    component: Article
+    component: () => import(/* webpackChunkName: "article" */ '../views/Blog/Article.vue'),
+    // component: Article
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'not-found',
+    // eslint-disable-next-line
     beforeEnter: (to, from, next) => {
-      console.log(to)
-      console.log(from)
       next(`/404`)
     }
   },
   {
     path: '/404',
     name: '404',
-    component: NotFoundView
+    component: () => import(/* webpackChunkName: "404" */ '../views/NotFoundView.vue'),
+    // component: NotFoundView
   }
 ]
 
